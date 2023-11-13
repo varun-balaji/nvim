ed30909ea6e0b2de28307be998dc2fd81bf0ef20
Varun Balaji <varun@lightbeam.ai>
2023-11-13 21:39:51 +0530

    Add fugitive keymaps and other git keymaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,16 @@
 return {
-  "tpope/vim-fugitive",
-  -- TODO: Add keybinds and opts
+  {
+    "tpope/vim-fugitive",
+    keys = {
+      { "<leader>gs", "<cmd>Git<CR>", desc = "Status (fugitive)" },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<leader>gf", "<cmd>Telescope git_status<CR>", desc = "Changed files" },
+      { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
+      { "<leader>gs", false },
+    },
+  },
 }
