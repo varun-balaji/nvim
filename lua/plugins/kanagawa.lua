bf7a08a999e3a8eefbaa20db5a8abe97c8356b74
Varun Balaji <varun@lightbeam.ai>
2023-11-22 16:57:10 +0530

    Kanagawa theme additions

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -7,9 +7,49 @@ return {
     -- name = "kanagawa",
     -- Commented
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("kanagawa")
-    end,
+    -- TODO: What if I comment out the config function?
+    -- config = function()
+    --   vim.cmd.colorscheme("kanagawa")
+    -- end,
+    opts = {
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          -- Transparent floating windows
+          -- https://github.com/rebelot/kanagawa.nvim#transparent-floating-windows
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+
+          -- Save an hlgroup with dark background and dimmed foreground
+          -- so that you can use it where your still want darker windows.
+          -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+          -- Popular plugins that open floats will link to NormalFloat by default;
+          -- set their background accordingly if you wish to keep them dark and borderless
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+          -- borderless Telescope
+          -- https://github.com/rebelot/kanagawa.nvim#borderless-telescope
+          TelescopeTitle = { fg = theme.ui.special, bold = true },
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+          -- Dark completion menu
+          -- https://github.com/rebelot/kanagawa.nvim#dark-completion-popup-menu
+          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+          PmenuSbar = { bg = theme.ui.bg_m1 },
+          PmenuThumb = { bg = theme.ui.bg_p2 },
+        }
+      end,
+    },
   },
   {
     "LazyVim/LazyVim",
--- todo.md
+++ todo.md
@@ -5,5 +5,5 @@
 - [ ] Fix supertab behaviour: tab to confirm instead of enter
 - [ ] Disable pyright and use only ruff-lsp.
 - [ ] Create an upstream repo. Also for dotfiles.
-- [ ] Update the kanagawa file with telescope changes from the repo.
+- [x] Update the kanagawa file with telescope changes from the repo.
 - [ ] TJ's method of executing lua (and maybe python) code from a buffer.
