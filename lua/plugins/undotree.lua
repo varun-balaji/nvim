9e1788b0e75ea8e692d09138e9f92724a158f3c5
Varun Balaji <varun@lightbeam.ai>
2023-11-22 18:38:47 +0530

    Fix undotree preview

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -14,10 +14,11 @@ return {
     opts = {
       extensions = {
         undo = {
-          side_by_side = true,
-          layout_strategy = "vertical",
+          -- side_by_side = true,
+          -- layout_strategy = "vertical",
           layout_config = {
-            preview_height = 0.8,
+            preview_width = 0.6,
+            -- preview_height = 0.8,
           },
         },
       },
