bcea553a84ea34cd63d0d85d83b669228226d99d
Varun Balaji <varun@lightbeam.ai>
2023-11-25 14:56:41 +0530

    Add autopep8

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -11,4 +11,19 @@ return {
       },
     },
   },
+  -- Setup autopep8 instead
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "autopep8")
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = { "autopep8" },
+      },
+    },
+  },
 }
