150d99812ec44ed4f60aa01e83ab57d687d8393b
Varun Balaji <varun@lightbeam.ai>
2023-12-04 14:48:14 +0530

    Add autocmd to not show diagnostics in insert mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Display diagnostics as virtual text only if not in insert mode
+vim.api.nvim_create_autocmd("InsertEnter", {
+  pattern = "*",
+  callback = function()
+    vim.diagnostic.config({ virtual_text = false })
+  end,
+})
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  callback = function()
+    vim.diagnostic.config({ virtual_text = true })
+  end,
+})
